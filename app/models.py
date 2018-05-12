a26707b7aec3cbac7f0ea8cf1e519c83460d55ac
Omar El-Mohri <omarelmohri@ipconnex.com>
2018-05-11 21:40:11 -0400

    advanced models

--- app/models.py
+++ app/models.py
@@ -1,10 +1,6 @@
 from app import db
 
 
-class Book(object):
-    pass
-
-
 class User(db.DynamicDocument):
     email = db.StringField(required=True, unique=True)
     # hash_pw = db.BinaryField(required=True)
@@ -12,10 +8,28 @@ class User(db.DynamicDocument):
     firstname = db.StringField(default='')
     lastname = db.StringField(default='')
 
+    books = db.ListField(db.ObjectIdField())
+
+    city = db.StringField()
+
     meta = {'collection': 'users'}
+
+    @classmethod
+    def users_count(cls) -> int:
+        return User.objects().count()
 
 
 class Book(db.DynamicDocument):
+    _id = db.StringField(max_length=20)
     isbn = db.StringField(required=True)
+    title = db.StringField()
+    language = db.StringRield()
+
+    amazon_url = db.StringField(default=None)
+    goodreads_url = db.StringField(default=None)
 
     meta = {'collection': 'books'}
+
+    @classmethod
+    def books_count(cls) -> int:
+        return Book.objects().count()
